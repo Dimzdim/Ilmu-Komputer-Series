71442b9d0c5ebdbd3bbcba0ec4f6fd0d08963c79
Dimas Purnomo <dimzpurnomo@gmail.com>
2021-03-27 12:01:25 +0700

    Tugas 2.1

---
+++ Mobile Programing/Tugas 2.1/lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'DimasApp',
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: new Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+          new Container(
+            width: 100,
+            color: Colors.red,
+          ),
+          Column(mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              new Container(
+                width: 100,
+                height: 100,
+                color: Colors.yellow,
+              ),
+              new Container(
+                width: 100,
+                height: 100,
+                color: Colors.green,
+              ),
+            ],
+          ),
+          new Container(
+            width: 100,
+            color: Colors.blue,
+          ),
+        ]),
+      ),
+    );
+  }
+}
